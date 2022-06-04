9fb9fbfd937c7e0bc7ecaae6625fdcf0b056719f
Đông Trần Ngọc <57401513+ngocdong1999@users.noreply.github.com>
2022-06-04 18:16:52 +0700

    final 1

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+
+const slider = tns({
+    container: ".slider__content",
+    prevButton: ".slider__btn.btn--left",
+    nextButton: ".slider__btn.btn--right",
+    items: 4,
+    mouseDrag: true,
+    swipeAngle: false,
+    speed: 400
+})
