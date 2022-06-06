543b225d959393a198f109ec04b2fc677672eeeb
Đông Trần Ngọc <57401513+ngocdong1999@users.noreply.github.com>
2022-06-06 14:50:39 +0700

    responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,33 @@ const slider = tns({
     prevButton: ".slider__btn.btn--left",
     nextButton: ".slider__btn.btn--right",
     items: 4,
+    nav: false,
     mouseDrag: true,
     swipeAngle: false,
-    speed: 400
+    speed: 400,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 4
+        }
+      }
 })
+
+const hamburger = document.querySelector('.header__nav-hamburger');
+const mobileMenu = document.querySelector('.header__nav-list');
+const mobileItem = document.querySelectorAll('.header__nav-list li');
+
+
+hamburger.addEventListener('click',() =>{
+    hamburger.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+});
+
+mobileItem.forEach((item) => {
+    item.addEventListener('click',() => {
+        hamburger.classList.toggle('active');
+        mobileMenu.classList.toggle('active');
+    });
+});
